6d7f76bf2dc3e624d3c8da8208e7b0742d2cecb7
Cai Guanhao (Choi Goon-ho) <caiguanhao@gmail.com>
2021-12-10 11:43:48 +0800

    move tests

---
+++ tests/query_test.go
@@ -0,0 +1,50 @@
+package psql_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/gopsql/db"
+	"github.com/gopsql/psql"
+)
+
+func testQuery(_t *testing.T, conn db.DB) {
+	t := test{_t}
+	model := psql.NewModelTable("", conn)
+	check := func(sql string, dest interface{}, expected string) {
+		rv := reflect.ValueOf(dest)
+		model.NewSQL(sql).MustQuery(&rv)
+		t.Helper()
+		t.String("test query: "+sql, fmt.Sprintf("%+v", rv.Elem().Interface()), expected)
+	}
+	check("SELECT 'a' UNION SELECT 'b'", &[]string{}, "[a b]")
+	check("SELECT 1 UNION SELECT 2", &[]int{}, "[1 2]")
+	check("SELECT 1, 'a' UNION SELECT 2, 'b'", &map[int]string{}, "map[1:a 2:b]")
+	check("SELECT 'a', 2 UNION SELECT 'b', 1", &map[string]int{}, "map[a:2 b:1]")
+	check("SELECT 1, 2, 'a', 'b' UNION SELECT 2, 3, 'c', 'd'", &map[int]struct {
+		id     int
+		status string
+		Name   string
+	}{}, "map[1:{id:2 status:a Name:b} 2:{id:3 status:c Name:d}]")
+	check("SELECT 1, 2, 'a' UNION SELECT 1, 3, 'd'", &map[int][]struct {
+		UserId int
+		status string
+	}{}, "map[1:[{UserId:2 status:a} {UserId:3 status:d}]]")
+	check("SELECT 1, 's', true, 0 UNION SELECT 1, 's', false, 1",
+		&map[int]map[string]map[bool]int{},
+		"map[1:map[s:map[false:1 true:0]]]")
+	check("SELECT 1, false, 0, 'hello' UNION SELECT 1, true, 1, 'world'", &map[int]map[bool]struct {
+		foo int
+		bar string
+	}{}, "map[1:map[false:{foo:0 bar:hello} true:{foo:1 bar:world}]]")
+	check("SELECT 1, 'a', 'b', 'c' UNION SELECT 2, 'd', 'e', 'f'", &map[int][3]string{}, "map[1:[a b c] 2:[d e f]]")
+	check("SELECT 1, 2, 'a', 'b' UNION SELECT 3, 4, 'c', 'd'", &map[[2]int][2]string{}, "map[[1 2]:[a b] [3 4]:[c d]]")
+	check("SELECT 1, 2, 'a', 'b' UNION SELECT 3, 4, 'c', 'd'", &map[struct {
+		uid int
+		id  int
+	}]struct {
+		status string
+		Name   string
+	}{}, "map[{uid:1 id:2}:{status:a Name:b} {uid:3 id:4}:{status:c Name:d}]")
+}
--- tests/sql_test.go
+++ tests/sql_test.go
@@ -228,6 +228,7 @@ func TestCRUDInPQ(t *testing.T) {
 		t.Fatal(err)
 	}
 	testCRUD(t, conn)
+	testQuery(t, conn)
 }
 
 func TestCRUDInPGX(t *testing.T) {
@@ -236,6 +237,7 @@ func TestCRUDInPGX(t *testing.T) {
 		t.Fatal(err)
 	}
 	testCRUD(t, conn)
+	testQuery(t, conn)
 }
 
 func TestCRUDInGOPG(t *testing.T) {
@@ -244,6 +246,7 @@ func TestCRUDInGOPG(t *testing.T) {
 		t.Fatal(err)
 	}
 	testCRUD(t, conn)
+	testQuery(t, conn)
 }
 
 func testCRUD(_t *testing.T, conn db.DB) {
@@ -369,85 +372,6 @@ func testCRUD(_t *testing.T, conn db.DB) {
 	t.Int("statuses length", rvStatuses.Elem().Len(), 2)
 	t.String("status 0", rvStatuses.Elem().Index(0).String(), "new")
 	t.String("status 1", rvStatuses.Elem().Index(1).String(), "new2")
-
-	var ids []int
-	model.Select("id").MustQuery(&ids)
-	t.Int("ids length", len(ids), 2)
-	t.Int("id 0", ids[0], 1)
-	t.Int("id 1", ids[1], 2)
-
-	id2status := map[int]string{}
-	model.Select("id, status").MustQuery(&id2status)
-	t.Int("map length", len(id2status), 2)
-	t.String("map 0", id2status[1], "new")
-	t.String("map 1", id2status[2], "new2")
-
-	var status2id map[string]int
-	model.Select("status, id").MustQuery(&status2id)
-	t.Int("map length", len(status2id), 2)
-	t.Int("map 0", status2id["new"], 1)
-	t.Int("map 1", status2id["new2"], 2)
-
-	var id2struct map[int]struct {
-		id     int
-		status string
-		Name   string
-	}
-	model.Select("user_id, id, status, name").MustQuery(&id2struct)
-	t.Int("map length", len(id2struct), 2)
-	t.String("struct string", fmt.Sprintf("%+v", id2struct[1]), "{id:1 status:new Name:foobar}")
-	// map[0:{id:2 status:new2 Name:} 1:{id:1 status:new Name:foobar}]
-
-	var id2structs map[int][]struct {
-		UserId int
-		status string
-	}
-	model.Select("1, user_id, status").MustQuery(&id2structs)
-	t.Int("map length", len(id2structs), 1)
-	t.String("struct string", fmt.Sprintf("%+v", id2structs[1]), "[{UserId:1 status:new} {UserId:0 status:new2}]")
-	// map[1:[{UserId:1 status:new} {UserId:0 status:new2}]]
-
-	var mapmap map[int]map[string]map[bool]int
-	model.NewSQL("SELECT 1, 's', true, 0 UNION SELECT 1, 's', false, 1").MustQuery(&mapmap)
-	t.String("struct string", fmt.Sprintf("%+v", mapmap), "map[1:map[s:map[false:1 true:0]]]")
-	model.NewSQL("SELECT 1, 't', false, 0").MustQuery(&mapmap)
-	t.String("struct string", fmt.Sprintf("%+v", mapmap), "map[1:map[s:map[false:1 true:0] t:map[false:0]]]")
-	var mapmapstruct map[int]map[bool]struct {
-		foo int
-		bar string
-	}
-	model.NewSQL("SELECT 1, false, 0, 'hello' UNION SELECT 1, true, 1, 'world'").MustQuery(&mapmapstruct)
-	t.String("struct string", fmt.Sprintf("%+v", mapmapstruct), "map[1:map[false:{foo:0 bar:hello} true:{foo:1 bar:world}]]")
-
-	// map[int][]interface{}: gopg returns error pg: Scan(nil)
-	// https://github.com/go-pg/pg/blob/v10.9.0/types/scan.go#L55
-	var id2strs map[int][3]string
-	model.Select("user_id, id::text, status, name").MustQuery(&id2strs)
-	t.Int("map length", len(id2strs), 2)
-	t.String("strs string", fmt.Sprintf("%+v", id2strs[1]), "[1 new foobar]")
-	// map[0:[2 new2 ] 1:[1 new foobar]]
-
-	var array2strs map[[2]int][2]string
-	model.Select("user_id, id, status, name").MustQuery(&array2strs)
-	t.Int("map length", len(array2strs), 2)
-	t.String("array string", fmt.Sprintf("%+v", array2strs[[2]int{1, 1}]), "[new foobar]")
-	// map[[0 2]:[new2 ] [1 1]:[new foobar]]
-
-	var struct2struct map[struct {
-		uid int
-		id  int
-	}]struct {
-		status string
-		Name   string
-	}
-	model.Select("user_id, id, status, name").MustQuery(&struct2struct)
-	t.Int("map length", len(struct2struct), 2)
-	k := struct {
-		uid int
-		id  int
-	}{1, 1}
-	t.String("struct string", fmt.Sprintf("%+v", struct2struct[k]), "{status:new Name:foobar}")
-	// map[{uid:0 id:2}:{status:new2 Name:} {uid:1 id:1}:{status:new Name:foobar}]
 
 	var createdAts []time.Time
 	model.Select("created_at").MustQuery(&createdAts)
